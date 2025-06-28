d2920a29a80ee0697237a461655751e6029072bf
KARIUKI-07 <kelvinkmwangi07@gmail.com>
2025-06-28 16:01:27 +0300

    latet commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import LuxeShop from './App'
+import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <LuxeShop />
+    <App />
   </React.StrictMode>
 )
